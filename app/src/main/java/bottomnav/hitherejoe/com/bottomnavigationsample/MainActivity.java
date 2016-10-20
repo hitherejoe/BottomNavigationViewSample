21ce4a87a52add0622084847672ce355a4f3805a
Joe Birch <hitherejoebirch@googlemail.com>
2016-10-20 15:21:56 +0100

    Update views from view pager

--- app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/MainActivity.java
+++ app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/MainActivity.java
@@ -2,20 +2,26 @@ package bottomnav.hitherejoe.com.bottomnavigationsample;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textFavorites;
+    private TextView textSchedules;
+    private TextView textMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
-        viewPager.setAdapter(new ViewPagerAdapter(this));
+        textFavorites = (TextView) findViewById(R.id.text_favorites);
+        textSchedules = (TextView) findViewById(R.id.text_schedules);
+        textMusic = (TextView) findViewById(R.id.text_music);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
@@ -26,13 +32,19 @@ public class MainActivity extends AppCompatActivity {
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
                             case R.id.action_favorites:
-                                viewPager.setCurrentItem(0);
+                                textFavorites.setVisibility(View.VISIBLE);
+                                textSchedules.setVisibility(View.GONE);
+                                textMusic.setVisibility(View.GONE);
                                 break;
                             case R.id.action_schedules:
-                                viewPager.setCurrentItem(1);
+                                textFavorites.setVisibility(View.GONE);
+                                textSchedules.setVisibility(View.VISIBLE);
+                                textMusic.setVisibility(View.GONE);
                                 break;
                             case R.id.action_music:
-                                viewPager.setCurrentItem(2);
+                                textFavorites.setVisibility(View.GONE);
+                                textSchedules.setVisibility(View.GONE);
+                                textMusic.setVisibility(View.VISIBLE);
                                 break;
                         }
                         return false;
