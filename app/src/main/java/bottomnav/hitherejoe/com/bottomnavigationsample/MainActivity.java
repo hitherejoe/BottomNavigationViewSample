75320ad288745ea8493291a8e924df3abcb27999
Juan <chavesjuan400@gmail.com>
2016-12-07 17:11:22 -0300

    Fixed animation on ...design:25.0.1
    In com.android.support:design:25.0.1, if _return_ is set to _false_ the animation is bugged. See here: https://code.google.com/p/android/issues/detail?id=227801&sort=-opened&colspec=ID%20Status%20Priority%20Owner%20Summary%20Stars%20Reporter%20Opened

--- app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/MainActivity.java
+++ app/src/main/java/bottomnav/hitherejoe/com/bottomnavigationsample/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                                 textMusic.setVisibility(View.VISIBLE);
                                 break;
                         }
-                        return false;
+                        return true;
                     }
                 });
     }
